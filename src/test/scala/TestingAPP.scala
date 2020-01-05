8e87e7d7470388d30a5213edd2022b0c6d7c6a9f
Dmitryi Black <59228406+DmitriyDimachine@users.noreply.github.com>
2020-01-05 10:20:15 +0200

    first app

---
+++ src/test/scala/TestingAPP.scala
@@ -0,0 +1,13 @@
+import scala.io.StdIn
+object TestingAPP extends App {
+  println("Press Enter")
+  StdIn.readLine("Hi")
+    print("Hey! What's up? \n My name is DIMACHINE. I am a test dialog bot, pretty boring and stupid, but what can you do :) ")
+    println("Okay, I told you about me. It's your turn! What's your name?")
+  val name = StdIn.readLine("Your name: ")
+    println(s"Wow, it's a pleasure $name !")
+    println("Maybe we can talk about something?")
+  println("A) Why wouldn't I? ")
+  println("B) Why wouldn't I? ")
+  println("C) Why wouldn't I? ")
+  }
